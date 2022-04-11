cb741f140434744857c1cf00f316694b64b0ee29
Tiago Pardal <tiagopardl@hotmail.com>
2022-04-11 01:06:11 +0100

    new implementation of the proposed model
    Updated Create Model and corresponding Remove Model for new implementation
    Missing some restrictions

--- F1/create.sql
+++ F1/create.sql
@@ -1,48 +1,73 @@
 BEGIN transaction;
-	--Cliente_Particular(CC, NIF, nome, morada, telefone, ref cliente)
-	create table IF NOT EXISTS Cliente_Particular(
-		CC int primary key,
-		NIF int,
-		nome varchar(20),
-		morada varchar(150),
-		telefone varchar(10),
-		refcliente int,
-		FOREIGN KEY (refcliente)
-     		REFERENCES Cliente_Particular (CC) ON DELETE CASCADE ON UPDATE cascade
-	);
-
-	--Cliente_Institucional(NIF, nome, morada, telefone, nome de contacto)
-	create table IF NOT EXISTS Cliente_Institucional(
+	--Cliente(NIF, nome, morada, telefone, ref cliente)
+	create table IF NOT EXISTS Cliente(
 		NIF int primary key,
 		nome varchar(20),
 		morada varchar(150),
 		telefone varchar(10),
-		nome_de_contacto varchar(20),
-		refcliente int,
-		FOREIGN KEY (refcliente)
-     		REFERENCES Cliente_Particular (CC) ON DELETE CASCADE ON UPDATE cascade
+		ref_cliente int--,
+		--FOREIGN KEY (ref_cliente)
+     	--	REFERENCES Cliente_Particular (CC) ON DELETE CASCADE ON UPDATE cascade
+	);
+	--Cliente_Particular(CC, cliente)
+	create table IF NOT EXISTS Cliente_Particular(
+		CC int primary key,
+		cliente int,
+		FOREIGN KEY (cliente)
+     		REFERENCES Cliente (NIF) ON DELETE CASCADE ON UPDATE cascade
+	);
+	--Cliente_Institucional(nome de contacto, cliente)
+	create table IF NOT EXISTS Cliente_Institucional(
+		nome_contacto varchar(20),
+		cliente int primary key,
+		FOREIGN KEY (cliente)
+     		REFERENCES Cliente (NIF) ON DELETE CASCADE ON UPDATE cascade
 	);
 
+	--Equipamento(id, estado)
+	create table IF NOT EXISTS Equipamento_Eletronico(
+		id int primary key,
+		estado varchar(15),
+		constraint estado check (estado IN ('Activo', 'PausaDeAlarmes', 'Inactivo'))
+	);
 	--Coordenadas(id, latitude, longitude)
 	create table IF NOT EXISTS Coordenadas(
 		id int primary key,
 		latitude numeric(3,1),
 		longitude numeric(3,1)
 	);
-
-	--Frota Veiculos(Cliente)
-
-	--Veiculo(matricula, nome, telefone condutor atual, id equipamento, estado equipamento)
+	--Bip Equipamento Eletronico(id, equipamento, marca temp, coordenadas)
+	create table IF NOT EXISTS Bip_Equipamento_Eletronico(
+		id int,
+		equipamento int,
+		marca_temporal date, 
+		coordenadas int,
+		primary key(id, equipamento),
+		FOREIGN KEY (equipamento)
+     		REFERENCES Equipamento_Eletronico (id) ON DELETE CASCADE ON UPDATE cascade,
+     	FOREIGN KEY (coordenadas)
+     		REFERENCES Coordenadas (id) ON DELETE CASCADE ON UPDATE cascade
+	);
+	--Condutor(nome, contacto)
+	create table IF NOT EXISTS Condutor(
+		id int primary key, 
+		nome varchar(20),
+		contacto varchar(10)
+	);
+	--Veiculo(matricula, condutor, equipamento, cliente)
 	create table IF NOT EXISTS Veiculo(
 		matricula varchar(6) primary key,
-		nome varchar(20),
-		telefone_condutor_atual varchar(10),
-		id_equipamento int,
-		estado_equipamento varchar(15),
-		constraint estado_equipamento check (estado_equipamento IN ('Activo', 'PausaDeAlarmes', 'Inactivo'))
+		condutor int,
+		equipamento int,
+		cliente int,
+		FOREIGN KEY (condutor)
+     		REFERENCES Condutor (id) ON DELETE CASCADE ON UPDATE cascade,
+     	FOREIGN KEY (equipamento)
+     		REFERENCES Equipamento_Eletronico (id) ON DELETE CASCADE ON UPDATE cascade,
+     	FOREIGN KEY (cliente)
+     		REFERENCES Cliente (NIF) ON DELETE CASCADE ON UPDATE cascade
 	);
-
-	--Zona Verde(coordenadas, raio)
+	--Zona Verde(veiculo, coordenadas, raio)
 	create table IF NOT EXISTS Zona_Verde(
 		coordenadas int,
 		veiculo varchar(6),
@@ -54,19 +79,5 @@ BEGIN transaction;
      		REFERENCES Veiculo (matricula) ON DELETE CASCADE ON UPDATE cascade
 	);
 
-
-	
-	--Bip Equipamento Eletronico(id, marca temp, coordenadas)
-	create table IF NOT EXISTS Bip_Equipamento_Eletronico(
-		id int,
-		veiculo varchar(6),
-		marca_temporal date, 
-		coordenadas int,
-		primary key(id, veiculo),
-		FOREIGN KEY (veiculo)
-     		REFERENCES Veiculo (matricula) ON DELETE CASCADE ON UPDATE cascade,
-     	FOREIGN KEY (coordenadas)
-     		REFERENCES Coordenadas (id) ON DELETE CASCADE ON UPDATE cascade
-	);
 
 commit transaction;
--- F1/remove.sql
+++ F1/remove.sql
@@ -1,7 +1,11 @@
 --remove modelo fisico
-DROP TABLE IF EXISTS Bip_Equipamento_Eletronico;
 DROP TABLE IF EXISTS Zona_Verde;
 DROP TABLE IF EXISTS Veiculo;
+DROP TABLE IF EXISTS Condutor;
+DROP TABLE IF EXISTS Bip_Equipamento_Eletronico;
 DROP TABLE IF EXISTS Coordenadas;
+DROP TABLE IF EXISTS Equipamento;
 DROP TABLE IF EXISTS Cliente_Institucional;
 DROP TABLE IF EXISTS Cliente_Particular;
+DROP TABLE IF EXISTS Cliente;
+
