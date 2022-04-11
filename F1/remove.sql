63fd3e17ea171d8e4f5c3ec92e0bccee36eb389e
Tiago Pardal <tiagopardl@hotmail.com>
2022-04-11 01:22:21 +0100

    small fix

--- F1/remove.sql
+++ F1/remove.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS Veiculo;
 DROP TABLE IF EXISTS Condutor;
 DROP TABLE IF EXISTS Bip_Equipamento_Eletronico;
 DROP TABLE IF EXISTS Coordenadas;
-DROP TABLE IF EXISTS Equipamento;
+DROP TABLE IF EXISTS Equipamento_Eletronico;
 DROP TABLE IF EXISTS Cliente_Institucional;
 DROP TABLE IF EXISTS Cliente_Particular;
 DROP TABLE IF EXISTS Cliente;
